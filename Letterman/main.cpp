97da6eae2a528a39be0d45820856d1df0a235a73
Lauren Annes <laurenannes@Laurens-MacBook-Air.local>
2015-01-24 19:23:19 -0500

    Initial Commit

---
+++ Letterman/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Letterman
+//
+//  Created by Lauren Annes on 1/24/15.
+//  Copyright (c) 2015 Lauren Annes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
